5c68c6dc5bdf1657b60e64b10d06980e325541d9
jcontour <contj186@newschool.edu>
2019-08-29 11:38:01 -0400

    movement lesson

---
+++ W1 Movement/Assets/MoreMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreMovement : MonoBehaviour
+{
+    public float speed;
+    public GameObject otherSquare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector2(Mathf.Sin(Time.time), 1);
+
+        transform.Translate(new Vector2(5, 0) * Time.deltaTime);
+        if (transform.position.x >= 19) {
+            transform.position = new Vector2(-19, transform.position.y);
+        }
+
+
+        //Vector2 dirBtwnSquares = otherSquare.transform.position - transform.position;
+        //float angle = Mathf.Atan2(dirBtwnSquares.y, dirBtwnSquares.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+
+    }
+}
---
+++ W1 Movement/Assets/MyMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float timer;
+    public GameObject circle;
+    float posx;
+    float posy;
+    public float freq;
+    public float amp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log(transform.position);
+        transform.position = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+        speed = 5;
+        timer = 0;
+        circle.transform.position = new Vector2(0, 0);
+        freq = 1;
+        amp = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(new Vector2(1, 0)*Time.deltaTime*speed);
+
+        if (timer >= 1)
+        {
+            transform.position = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+
+        posx = Mathf.Sin(Time.time*freq)*amp;
+        posy = Mathf.Cos(Time.time*freq)*amp;
+        Debug.Log(posx);
+
+        circle.transform.position = new Vector2(posx + transform.position.x, posy + transform.position.y);
+        
+    }
+}
