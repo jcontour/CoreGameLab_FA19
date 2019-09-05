9dffff4a1657ac9d2a042914e030eeb2ae1493a8
jcontour <contj186@newschool.edu>
2019-09-05 11:01:28 -0400

    physics cont. triggers, foreach, boolean states

---
+++ W2 Physics/Assets/Flipper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    bool isGravityDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGravityDown = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGravityDown) {
+            rb.gravityScale = 1;
+
+        }
+        else {
+            rb.gravityScale = -1;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGravityDown = !isGravityDown;
+    }
+}
---
+++ W2 Physics/Assets/TriggerZone.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float forceamount;
+    public GameObject[] planets = new GameObject[4];
+    Vector3 planet_pos;
+    Vector3 direction;
+    float distance;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.left * 10, ForceMode2D.Impulse);
+        distance = Vector3.Distance(planets[0].transform.position, transform.position);
+        planet_pos = planets[0].transform.position;
+    }
+
+    private void Update()
+    {
+         
+       foreach (GameObject planet in planets) {
+            float distcheck = Vector3.Distance(planet.transform.position, transform.position);
+            if (distcheck < distance) {
+                planet_pos = planet.transform.position;
+                distance = distcheck;
+            }
+        }
+
+        direction = planet_pos - transform.position;
+        rb.AddForce(direction * forceamount);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        rb.AddForce(Vector2.up * forceamount);
+    }   
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+        
+    //}
+
+}
