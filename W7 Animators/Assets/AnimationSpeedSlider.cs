68260bac62228e5e37dbb3cb49b636f302dbca62
jcontour <contj186@newschool.edu>
2019-10-08 11:40:53 -0400

    animation examples

---
+++ W7 Animators/Assets/AnimationSpeedSlider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationSpeedSlider : MonoBehaviour
+{
+
+    public Slider s;
+    Animator anim;
+
+    float s_value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        s_value = s.value;
+        anim.speed = s_value;
+    }
+
+    public void myAnimationEventFunction() {
+        Debug.Log("I'm dizzy!");
+    }
+}
---
+++ W7 Animators/Assets/DoorOpening.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpening : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        anim.SetTrigger("doDoorOpen");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        anim.SetTrigger("doDoorOpen");
+    }
+
+
+
+}
