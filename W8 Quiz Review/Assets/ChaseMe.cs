a87a5bac2dc295ce98b0cf1fc6def3cfe0de4f87
jcontour <contj186@newschool.edu>
2019-10-22 11:36:27 -0400

    quiz review and post processing demo

---
+++ W8 Quiz Review/Assets/ChaseMe.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseMe : MonoBehaviour
+{
+
+    float speed;
+    Vector3 mousePos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = GameManagement.Will.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouseWorldPos = new Vector3(mousePos.x, mousePos.y, 0);
+
+        transform.position = Vector2.MoveTowards(transform.position, mouseWorldPos, speed * Time.deltaTime);
+
+    }
+}
---
+++ W8 Quiz Review/Assets/GameManagement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour
+{
+
+    public static GameManagement Will;
+
+    private void Awake()
+    {
+        if (Will == null)
+        {
+            Will = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (Will != this) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public float speed;
+    public Slider speedSlider;
+    public Text buttonText;
+
+    public void SliderChange() {
+        speed = speedSlider.value;
+        buttonText.text = speed.ToString();
+    }
+
+    public void SceneChange(int SceneNum) {
+        SceneManager.LoadScene(SceneNum);
+    }
+    
+}
